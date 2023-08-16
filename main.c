770838c94ccb431374c9523e26e8276ea4b423a9
alessandrobertelli <bertellialessandro02@gmail.com>
2023-08-16 14:59:57 +0200

    implementing linkedList 1.1

--- main.c
+++ main.c
@@ -8,19 +8,32 @@ struct Stazione {
     struct Stazione *next;
     struct Stazione *prev;
 };
-
-struct Stazione *trovaStazione(struct Stazione **testa, int km) {
+/*
+struct Stazione *trovaStazione(struct Stazione **testa, struct Stazione **coda, int km) {
     if ((*testa)->km == km)
         return (*testa);
-    while ((*testa)->next != NULL) {
-        (*testa) = (*testa)->next;
-        if ((*testa)->km == km)
-            return (*testa);
+    if ((*testa)->km > km)
+        return NULL;
+    if ((*coda)->km == km)
+        return (*coda);
+    if ((*coda)->km < km) {
+        return NULL;
+    }
+
+    struct Stazione *temp = (struct Stazione *) malloc(sizeof(struct Stazione));
+    temp = *testa;
+
+    while (temp->next != NULL) {
+        temp = temp->next;
+        if (temp->km == km){
+            free(temp);
+            return ;
+        }
     }
     return NULL;
-}
+}*/
 
-void inserisci(struct Stazione **testa, int km) {
+void insStazione(struct Stazione **testa, struct Stazione **coda, int km) {
 
     struct Stazione *newStazione = (struct Stazione *) malloc(sizeof(struct Stazione));
     newStazione->km = km;
@@ -29,52 +42,80 @@ void inserisci(struct Stazione **testa, int km) {
 
     struct Stazione *temp = (struct Stazione *) malloc(sizeof(struct Stazione));
 
-    if (*testa == NULL)
+    if (*testa == NULL) {
         *testa = newStazione;
-
-    else if ((*testa)->km >= newStazione->km) {
+        *coda = newStazione;
+        printf("aggiunta\n");
+    } else if ((*testa)->km > newStazione->km) {
         newStazione->next = *testa;
         newStazione->next->prev = newStazione;
         *testa = newStazione;
-
+        printf("aggiunta\n");
     } else {
         temp = *testa;
 
-        // locate the node after which the new node
-        // is to be inserted
-        while (temp->next != NULL && temp->next->km < newStazione->km)
+        while (temp->next != NULL && temp->next->km < newStazione->km) {
             temp = temp->next;
+        }
+        if (temp->km == newStazione->km) {
+            printf("non aggiunta\n");
+            //free(newStazione);
+        } else {
+            newStazione->next = temp->next;
 
-        newStazione->next = temp->next;
-
-        if (newStazione->next != NULL)
-            newStazione->next->prev = newStazione;
-
-        temp->next = newStazione;
-        newStazione->prev = temp;
+            if (newStazione->next != NULL) {
+                newStazione->next->prev = newStazione;
+            } else {
+                *coda = newStazione;
+            }
+            temp->next = newStazione;
+            newStazione->prev = temp;
+            printf("aggiunta\n");
+        }
     }
     free(temp);
 }
 
-void eliminaStazione(struct Stazione **testa, struct Stazione *del_stazione) {
+void delStazione(struct Stazione **testa, struct Stazione **coda, int km) {
 
-    if (*testa == NULL || del_stazione == NULL)
+    if ((*testa)->km > km) {
+        printf("non demolita");
         return;
+    }
+    if ((*coda)->km < km) {
+        printf("non demolita");
+        return;
+    }
+    struct Stazione *temp = (struct Stazione *) malloc(sizeof(struct Stazione));
+    struct Stazione *del_stazione = (struct Stazione *) malloc(sizeof(struct Stazione));
+    temp = *testa;
 
-    // if del_Stazione is the head Stazione, point the head pointer to the next of del_Stazione
+    while (temp->next != NULL) {
+        temp = temp->next;
+        if (temp->km == km){
+            del_stazione=temp;
+            free(temp);
+        }
+    }
+
+    if (*testa == NULL || del_stazione == NULL) {
+        printf("non demolita\n");
+        return;
+    }
+
     if (*testa == del_stazione)
         *testa = del_stazione->next;
+    if (*coda == del_stazione)
+        *coda = del_stazione->prev;
 
-    // if del_Stazione is not at the last Stazione, point the prev of Stazione next to del_Stazione to the previous of del_Stazione
-    if (del_stazione->next != NULL)
+    if (del_stazione->next != NULL) {
         del_stazione->next->prev = del_stazione->prev;
-
-    // if del_Stazione is not the first Stazione, point the next of the previous Stazione to the next Stazione of del_Stazione
+    }
     if (del_stazione->prev != NULL)
         del_stazione->prev->next = del_stazione->next;
 
-    // free the memory of del_Stazione
     free(del_stazione);
+    printf("demolita\n");
 }
 
 // print the doubly linked list
@@ -94,6 +135,15 @@ int main() {
     struct Stazione *testa = NULL;
     struct Stazione *coda = NULL;
 
+    insStazione(&testa, &coda, 1);
+
+    insStazione(&testa, &coda, 4);
+
+    insStazione(&testa, &coda, 8);
+
+    insStazione(&testa, &coda, 19);
+
+    delStazione(&testa, &coda, 3);
 
 }
 
