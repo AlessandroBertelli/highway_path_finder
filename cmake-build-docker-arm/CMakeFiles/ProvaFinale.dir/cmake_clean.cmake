bae6915fc5cb1d52d4f057a5154be4603992f1ef
alessandrobertelli <bertellialessandro02@gmail.com>
2023-08-27 14:40:54 +0200

    after change computer commit with cmake profile

---
+++ cmake-build-docker-arm/CMakeFiles/ProvaFinale.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProvaFinale.dir/main.c.o"
+  "ProvaFinale"
+  "ProvaFinale.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ProvaFinale.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
