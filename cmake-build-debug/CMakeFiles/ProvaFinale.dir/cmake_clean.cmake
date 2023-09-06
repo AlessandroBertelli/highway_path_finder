acb22e3fe1c3123c9e6e5636916d191d976cc169
alessandrobertelli <bertellialessandro02@gmail.com>
2023-09-06 20:38:37 +0200

    last commit before implementing BFS with direct path

---
+++ cmake-build-debug/CMakeFiles/ProvaFinale.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProvaFinale.dir/main.c.o"
+  "CMakeFiles/ProvaFinale.dir/main.c.o.d"
+  "ProvaFinale"
+  "ProvaFinale.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ProvaFinale.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ProvaFinale.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for ProvaFinale.
+# This may be replaced when dependencies are built.
